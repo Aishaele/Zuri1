5c6b196782fd1f06b8ac07f9644bb136f14b79b3
iamaisha <nanaaishaeleamodu@gmail.com>
2022-10-29 04:29:01 -0700

    i made changes in the index.css

--- src/App.js
+++ src/App.js
@@ -1,34 +1,38 @@
 import profileimg from "./img1.jpg"
 import arrow from "./arrow.svg"
 import menu from "./menu.svg"
+import colors from "./colors.svg"
+import github from "./github.svg"
+import foo from "./footerimg.svg"
 
 function App() {
   return (
-    <div className="App">
-    <div class="hero">
-        <div class="wrapper">
-            <img class="arrow" src={arrow} alt=""/>
-            <img class="menu" src={menu} alt=""/>
-        <div class="profile">
+    <div classNameName="App">
+    <div className="hero">
+        <div className="wrapper">
+            <img className="arrow" src={arrow} alt=""/>
+            <img className="menu" src={menu} alt=""/>
+        <div className="profile">
             <img id='profile_img' src={profileimg} alt=""/>
             <h3 id='slack'>AishaEle Amodu</h3>
         </div>
-        <div class="style-a-tag">
-            <a id='Twitter' href="https://twitter.com/AishaeleA">Twitter Link</a>
-            <a id='books' href="https://training.zuri.team">Zuri Team</a>
-            <a id='Zuri Books' href="http://books.zuri.team">Zuri Book</a>
-            <a id='book_python' href="https://books.zuri.team/python-for-beginners?ref_id=nanaaishaeleamodu">Python Books</a>
-            <a id='pitch' href="https://background.zuri.team">Background Check for Coders</a>
-            <a id='book_design' href="https://bookszuri.team/design-rules">Design Books</a>
+        <div className="style-a-tag">
+          
+            <button><a id='Twitter' href="https://twitter.com/AishaeleA">Twitter Link</a></button>
+            <button><a id='books' href="https://training.zuri.team">Zuri Team</a></button>
+            <button><a id='Zuri Books' href="http://books.zuri.team">Zuri Book</a></button>
+            <button> <a id='book_python' href="https://books.zuri.team/python-for-beginners?ref_id=nanaaishaeleamodu">Python Books</a></button>
+            <button><a id='pitch' href="https://background.zuri.team">Background Check for Coders</a></button>
+            <button> <a id='book_design' href="https://bookszuri.team/design-rules">Design Books</a></button>
         </div>
-        <div class="style-logo">
-            <img class="" src="./colors.svg" alt=""/>
-            <img src="./github.svg" alt=""/>
+        <div className="style-logo">
+            <img className="" src= {colors} alt=""/>
+            <img src={github} alt=""/>
         </div>
         <footer>
-            <h3>Zuri<span class="dot">.</span>Internship</h3>
+            <h3>Zuri<span className="dot">.</span>Internship</h3>
             <h4> HNG Internship 9 Frontend Task</h4>
-            <img src="./footerimg.svg" alt=""/>
+            <img src={foo} alt=""/>
         </footer>
         </div>
     </div>
